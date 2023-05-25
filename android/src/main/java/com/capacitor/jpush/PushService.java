a4922d415f747b04a7cb55768352d434f3a9a2df
ZhuXian <18270244870@163.com>
2023-05-25 20:47:07 +0800

    chore: support v5

--- android/src/main/java/com/capacitor/jpush/PushService.java
+++
@@ -1,6 +0,0 @@
-package com.capacitor.jpush;
-
-import cn.jpush.android.service.JCommonService;
-
-public class PushService extends JCommonService {
-}
--- ios/Plugin/JPush.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import UserNotifications
-
-@objc public class JPush: NSObject {
-    
-}
