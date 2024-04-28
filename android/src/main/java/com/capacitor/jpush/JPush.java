99d1e020dc5f824e02b2086a87ee580b11b9638a
zhuxian <18270244870@163.com>
2024-04-28 23:17:35 +0800

    refactor: 优化Android代码

--- android/src/main/java/com/capacitor/jpush/JPush.java
+++ android/src/main/java/com/capacitor/jpush/JPush.java
@@ -1,3 +1,24 @@
 package com.capacitor.jpush;
 
-public class JPush {}
+import com.getcapacitor.JSArray;
+import com.getcapacitor.PluginCall;
+
+public class JPush {
+    public JSArray getTagsValue(PluginCall call) {
+        JSArray tags = call.getArray("tags", JSArray.from(String.class));
+        if (tags == null || tags.length() == 0) {
+            call.reject("tags参数不能为空");
+            return new JSArray();
+        }
+        return tags;
+    }
+
+    public Integer sequenceNumber(PluginCall call) {
+        int defaultValue = -1;
+        Integer sequence = call.getInt("sequence", defaultValue);
+        if(sequence != null) {
+            return sequence;
+        }
+        return defaultValue;
+    }
+}
