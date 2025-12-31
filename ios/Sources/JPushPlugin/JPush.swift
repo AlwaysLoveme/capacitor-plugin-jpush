257db21f2f0336a58def108672bcd96537475d0f
zhuxian <18270244870@163.com>
2025-12-31 17:02:14 +0800

    feat: merge main

---
+++ ios/Sources/JPushPlugin/JPush.swift
@@ -0,0 +1,17 @@
+import Capacitor
+import Foundation
+import UserNotifications
+
+@objc public class JPush: NSObject {
+    public func requestPermissions(with completion: ((Bool, Error?) -> Void)? = nil) {
+           UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+               completion?(granted, error)
+           }
+       }
+
+       public func checkPermissions(with completion: ((UNAuthorizationStatus) -> Void)? = nil) {
+           UNUserNotificationCenter.current().getNotificationSettings { settings in
+               completion?(settings.authorizationStatus)
+           }
+       }
+}
